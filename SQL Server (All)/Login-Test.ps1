ca3ceb50d86231b78c3ce14569aa02012f9cee66
Bryan Cooper <bryan.cooper@nuance.com>
2019-11-11 07:07:47 -0700

    fix some typos

--- SQL Server (All)/Login-Test.ps1
+++ SQL Server (All)/Login-Test.ps1
@@ -1,8 +1,8 @@
-$Logfile = "C:\Windows\Temp\loginUW.log"
+$Logfile = "C:\Windows\Temp\LoginTest.log"
 while (1 -eq 1) {
 	$Date = Get-Date -Format o
 	$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
-	$login = osql -S "[ServerName].database.windows.net" -U "[Username]" -Q "SELECT 1;" -d "[DatabaseName" -P "[Password]"
+	$login = osql -S "[ServerName].database.windows.net" -U "[Username]" -Q "SELECT 1;" -d "[DatabaseName]" -P "[Password]"
 	$stopwatch.Stop()
 	$Milliseconds = $stopwatch.ElapsedMilliseconds
 	Add-content $Logfile -value "$Date   $Milliseconds   $login"
