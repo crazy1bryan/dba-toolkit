f00136bec27105a9102fb50c6454ae28ec3f9cb9
Bryan Cooper <bryan.cooper@nuance.com>
2020-03-26 08:58:43 -0600

    Update script

--- SQL Server (All)/Index-Usage.sql
+++ SQL Server (All)/Index-Usage.sql
@@ -1,20 +1,29 @@
 SELECT
-	sc.name	AS SchemaName,
-	o.name AS ObjectName,
-	i.name AS IndexName,
-	i.index_id,
-	s.user_seeks,
-	s.user_scans, 
-	s.user_lookups,
-	s.user_seeks + s.user_scans + s.user_lookups AS TotalReads,
-	s.user_updates
-FROM sys.dm_db_index_usage_stats s 
-	JOIN sys.indexes i ON i.object_id = s.object_id AND i.index_id = s.index_id
-	JOIN sys.objects o ON o.object_id = i.object_id
-	JOIN sys.schemas sc ON sc.schema_id = o.schema_id
-WHERE
-	o.type = 'U'
-ORDER BY
-	SchemaName,
-	ObjectName,
-	IndexName;
+	OBJECT_NAME(IX.OBJECT_ID) AS Table_Name,
+	IX.name AS Index_Name,
+	IX.type_desc Index_Type,
+	SUM(PS.[used_page_count]) * 8 AS IndexSizeKB,
+	IXUS.user_seeks AS NumOfSeeks,
+	IXUS.user_scans AS NumOfScans,
+	IXUS.user_lookups AS NumOfLookups,
+	IXUS.user_updates AS NumOfUpdates,
+	IXUS.last_user_seek AS LastSeek,
+	IXUS.last_user_scan AS LastScan,
+	IXUS.last_user_lookup AS LastLookup,
+	IXUS.last_user_update AS LastUpdate
+FROM sys.indexes IX
+	INNER JOIN sys.dm_db_index_usage_stats IXUS ON IXUS.index_id = IX.index_id AND IXUS.OBJECT_ID = IX.OBJECT_ID
+	INNER JOIN sys.dm_db_partition_stats PS on PS.object_id=IX.object_id
+WHERE OBJECTPROPERTY(IX.OBJECT_ID,'IsUserTable') = 1
+GROUP BY
+	OBJECT_NAME(IX.OBJECT_ID),
+	IX.name,
+	IX.type_desc,
+	IXUS.user_seeks,
+	IXUS.user_scans,
+	IXUS.user_lookups,
+	IXUS.user_updates,
+	IXUS.last_user_seek,
+	IXUS.last_user_scan,
+	IXUS.last_user_lookup,
+	IXUS.last_user_update
