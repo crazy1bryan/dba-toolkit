bb127446a14598753feb426d61dbf84bad317303
Bryan Cooper <bryan.cooper@nuance.com>
2021-05-27 11:22:21 -0600

    Enhance index query

--- SQL Server (All)/Index-List.sql
+++ SQL Server (All)/Index-List.sql
@@ -1,13 +1,20 @@
 SELECT
-	SO.name,
-	SI.name,
-	*
-FROM
-	sys.indexes SI
-	INNER JOIN sys.objects SO ON SO.object_id = SI.object_id
-WHERE
-	SO.is_ms_shipped = 0 AND
-	SO.name NOT LIKE '%sysdiag%'
+	OBJECT_SCHEMA_NAME(i.[object_id]) AS SchemaName,
+	OBJECT_NAME(i.[object_id]) AS TableName,
+	i.[name] AS IndexName,
+	c.[name] AS ColumnName,
+	ic.is_included_column,
+	i.index_id,
+	i.type_desc,
+	i.is_unique,
+	i.data_space_id,
+	i.ignore_dup_key,
+	i.is_primary_key
+FROM sys.indexes i
+	INNER JOIN sys.index_columns ic ON ic.object_id = i.object_id AND i.index_id = ic.index_id
+	INNER JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
 ORDER BY
-	SO.name,
-	SI.name;
+	SchemaName,
+	TableName,
+	ic.index_id,
+	ic.index_column_id;
