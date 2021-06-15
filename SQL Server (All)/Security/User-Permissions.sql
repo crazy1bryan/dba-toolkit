27c8484067bd99b72c32f6b6cc41d18a16f6d8bf
Bryan Cooper <bryan.cooper@nuance.com>
2021-06-15 10:07:11 -0600

    Add a new query to pull database-level permissions for all users

--- SQL Server (All)/Security/User-Permissions.sql
+++ SQL Server (All)/Security/User-Permissions.sql
@@ -5,3 +5,16 @@ SELECT * FROM fn_my_permissions(NULL, 'DATABASE');
 EXECUTE AS USER = 'UserHere';
 SELECT * FROM fn_my_permissions(NULL, 'DATABASE');
 REVERT;
+
+-- Show all database-level permissions for all users
+SELECT
+	su.name,
+	dp.state_desc,
+	dp.permission_name
+FROM sys.database_permissions dp
+	INNER JOIN sysusers su on su.uid = dp.grantee_principal_id
+WHERE
+	dp.class = 0
+ORDER BY
+	su.name,
+	dp.state_desc;
